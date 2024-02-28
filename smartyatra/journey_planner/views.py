a4ffe8ac34eb218d5396c2509432178ac7e9551a
Jyoti Kumari <jyoti.kumari.mec20@itbhu.ac.in>
2024-02-29 02:18:57 +0530

    merge conflicts

--- smartyatra/journey_planner/views.py
+++ smartyatra/journey_planner/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from geopy.geocoders import Nominatim
 
 # Create your views here.
 def hpage(request):
     return render(request, "index.html")
 
 class planner(View):
+
+    def address_to_latlng(address):
+        geolocator = Nominatim(user_agent="myGeocoder")
+        location = geolocator.geocode(address)
+        if location:
+            return (location.latitude, location.longitude)
+        else:
+            return (None, None)
+
     def get(self,request):
         return render(request,'planner.html')
     
     def post(self,request):
+<<<<<<< HEAD
+        source = address_to_latlng(request.POST.get('source'))
+        destination = address_to_latlng(request.POST.get('destination'))
+
+=======
         source = request.POST.get('source')
         destination = request.POST.get('destination')
         arr1 = nearby_points(source)
@@ -19,6 +34,7 @@ class planner(View):
         res = search_algo(source,destination,arr1,arr2)
 
         return render(request,'planner.html',res)
+>>>>>>> 6616d74310cbd649b1280add96f6e1959f938310
 
 
     def search_algo(source,destination,arr1,arr2):
@@ -29,6 +45,9 @@ class planner(View):
 
     def duration(source,destination):
         pass
+
+    
+
 
 def about(request):
     return render(request,"about.html")

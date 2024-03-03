523fcbca52f555c8255517b5aa543f8c65cf61bb
Pratu07-prog <vpratiksha.chandrakant.mec20@itbhu.ac.in>
2024-03-03 18:08:50 +0530

    nearby-services backend

--- smartyatra/journey_planner/views.py
+++ smartyatra/journey_planner/views.py
@@ -3,9 +3,10 @@ from django.views import View
 from django.http import HttpResponse
 from geopy.geocoders import Nominatim
 import pandas as pd
-from .helper import find_nearest_points, search_algo
+from .helper import find_nearest_points, search_algo, find_nearby_services
 from django import forms
 from .models import Nodes, Edges
+import json
 
 
 def hpage(request):
@@ -38,12 +39,33 @@ class planner(View):
         arr2 = find_nearest_points(lat2, lon2, precision=7)
 
         routes = search_algo(lat1, lon1, lat2, lon2, arr1, arr2)
-        print(routes)
-        return render(request,'planner.html', {'routes': routes, 'source': source, 'destination': destination})
+        json_arr1 = json.dumps(routes)
+        
+        return render(request,'planner.html', {'routes': routes, 'source': source, 'destination': destination, 'json_data':json_arr1})
 
 
 def about(request):
     return render(request,"about.html")
 
-def nearby(request):
-    return render(request,'nearby.html')
+
+class nearby(View):
+    def get(self, request):
+        nodes = Nodes.objects.all()
+        return render(request,'nearby.html',{'nodes':nodes, 'services': 0})
+    
+    def post(self, request):
+        location = request.POST.get('location')
+
+        def get_name(node):
+            return node.name
+
+        df = pd.DataFrame(Nodes.objects.all(),columns=["Nodes"])
+        df['Name'] = df['Nodes'].apply(get_name)
+        location = df[df['Name'] == (location)]
+        lat = location.iloc[0]['Nodes'].lat
+        lon = location.iloc[0]['Nodes'].lon
+
+        arr1 = find_nearby_services(lat, lon, precision=5)
+        json_arr1 = json.dumps(arr1)
+        print(json_arr1)
+        return render(request,'nearby.html', {'services': json_arr1, 'location': location.iloc[0]['Nodes'].name})

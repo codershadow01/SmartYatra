b0fcbc809e5e13204ad3e63e6fcddc1bb232ced3
Shadow <yashitabansal123@gmail.com>
2024-03-03 18:58:06 +0530

    Database changed

--- smartyatra/smartyatra/settings.py
+++ smartyatra/smartyatra/settings.py
@@ -79,13 +79,22 @@ WSGI_APPLICATION = 'smartyatra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yatra',
+        'USER': 'djangouser',
+        'PASSWORD': 'project@2002',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

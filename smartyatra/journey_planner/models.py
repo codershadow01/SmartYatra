9873979a0d2ac42e86f5789188e0d9b4e084b862
Shadow <yashitabansal123@gmail.com>
2024-03-03 20:37:54 +0530

    Revert "models changed"
    This reverts commit 42637685086a5464cbaa47e34b10ee0c3d0a95f5.

--- smartyatra/journey_planner/models.py
+++ smartyatra/journey_planner/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Nodes(models.Model):
-    name = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=100)
     lat = models.FloatField()
     lon = models.FloatField()
     geohash = models.CharField(max_length=100)
